ff54419d3e72cf0c2b98e46f23e353223a881ada
camislly <106408346+camislly@users.noreply.github.com>
2022-11-09 00:06:44 -0300

    Add files via upload

---
+++ aula-07-atividade-main/ex027/script.js
@@ -0,0 +1,14 @@
+function tabuada() {
+    let saida = document.getElementById('saida')
+    let n = Number(document.getElementById('fnum').value)
+
+    saida.innerHTML = `<h2>Tabuada de ${n}</h2>`
+    let c = 1
+    while (c <= 10) {
+        saida.innerHTML += `${n} x ${c} = <strong>${n * c}</strong><br>`
+        c ++
+    }
+}
+function acao5() {
+    location. reload();
+}
